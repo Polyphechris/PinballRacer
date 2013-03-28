b7a870fbefa9b7c8b5abf7b379c6f8c65dbc4956
Matthew Tam <matthew.tam@live.com>
2013-03-28 17:56:30 -0400

    Adding classes responsible for drawing the Player and Track to help abstract code from the game class

---
+++ PinballRacer/PinballRacer/Player/PlayerSpriteManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PinballRacer.Player
+{
+    public class PlayerSpriteManager : Microsoft.Xna.Framework.DrawableGameComponent
+    {
+        public PlayerSpriteManager(Game1 game)
+            : base(game)
+        {
+            base.Initialize();
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        public override void Update(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void Draw(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ PinballRacer/PinballRacer/Track/TrackSpriteManager.cs
@@ -0,0 +1,36 @@
+﻿
+namespace PinballRacer.Track
+{
+    public class TrackSpriteManager : Microsoft.Xna.Framework.DrawableGameComponent
+    {
+
+        public TrackSpriteManager(Game1 game)
+            : base(game)
+        {
+            base.Initialize();
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        public override void Update(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void Draw(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+
+        
+        
+    }
+}
