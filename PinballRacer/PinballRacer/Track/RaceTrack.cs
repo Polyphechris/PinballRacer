ed3c46e3dd3bfc2636667d991d43fa7c4aee76df
Patrick <patrickmodafferi@gmail.com>
2013-04-18 04:34:06 -0400

    added world variable for all obstacles

--- PinballRacer/PinballRacer/Track/RaceTrack.cs
+++ PinballRacer/PinballRacer/Track/RaceTrack.cs
@@ -345,13 +345,7 @@ namespace PinballRacer.Track
         {
             PathController.Draw(view, projection, content.Load<Model>("ball"), content.Load<Model>("cube"));
             DrawFloor();
-            DrawWalls(view, projection, content.Load<Model>("cube"));
-            foreach (Floor f in floors)
-            {
-                f.draw(view, projection);
-                
-            }
-
+           // DrawWalls(view, projection, content.Load<Model>("cube"));
             foreach (Obstacle o in obstacles.Values)
             {
             //    if(o.GetType() != tokenWall.GetType())
